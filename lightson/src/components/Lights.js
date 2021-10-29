eb0bd32b5e3289cc0fd92c1565769029a972a78b
Afonso <afonsonunespedroso@gmail.com>
2021-10-29 13:59:24 +0100

    lights ooon

--- lightson/src/components/Lights.js
+++ lightson/src/components/Lights.js
@@ -1,9 +1,27 @@
 import React, { Component, useState } from 'react'
 
+import lightoff from "../assets/lightOff.png"
+import lighton from "../assets/lightOn.png"
+import "./light.scss"
 
 function Lights() {
-    return ( <div>
-        lights project
+
+    const[light,setLight]=useState(false)
+
+    
+
+    function handleClick(e){
+        setLight(!light)
+    }
+
+    return ( <div className="bd">
+        <div className="mainContainer">
+        <div className="container">
+            <h1></h1>
+            <button id="bt1" onClick={handleClick} className="buttonCute">ON/OFF</button>
+        </div>
+            {!light ? <img className="lights" src={lightoff} alt ="light off"/> : <img className="lights" src={lighton} alt ="light on"/>}
+        </div>
     </div> );
 }
 
