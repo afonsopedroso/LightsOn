e1ef0ecaa03b67a38de6b6015cceae4d9987f136
Afonso <afonsonunespedroso@gmail.com>
2021-10-29 11:11:02 +0100

    first commit

---
+++ lightson/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Lights from './components/Lights';
+
+function App() {
+  return (
+    <div className="App">
+      <Lights />
+    </div>
+  );
+}
+
+export default App;
---
+++ lightson/src/components/Lights.js
@@ -0,0 +1,10 @@
+import React, { Component, useState } from 'react'
+
+
+function Lights() {
+    return ( <div>
+        lights project
+    </div> );
+}
+
+export default Lights;
